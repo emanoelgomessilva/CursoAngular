17709ac4eccbd813f86c0caa514e527312547e7e
emanoelgomessilva <43941192+emanoelgomessilva@users.noreply.github.com>
2019-09-09 18:17:00 -0300

    Upload de imagens

--- ProAgilApp/src/app/_services/evento.service.ts
+++ ProAgilApp/src/app/_services/evento.service.ts
@@ -27,6 +27,14 @@ export class EventoService {
     return this.http.post(this.baseUrl, evento);
   }
 
+  postUpload(file: File, name: string) {
+    const fileToUpload = file[0] as File;
+    const formData = new FormData();
+    formData.append('file', fileToUpload, name);
+
+    return this.http.post(`${this.baseUrl}/upload`, formData);
+  }
+
   putEvento(evento: Evento) {
     return this.http.put(`${this.baseUrl}/${evento.id}`, evento);
   }
--- ProAgilApp/src/app/eventos/eventos.component.ts
+++ ProAgilApp/src/app/eventos/eventos.component.ts
@@ -28,6 +28,9 @@ export class EventosComponent implements OnInit {
   modoSalvar = 'post';
   bodyDeletarEvento = '';
   titulo = 'Eventos';
+  file: File;
+  fileNameToUpdate: string;
+  dataAtual: string;
 
   constructor(private eventoService: EventoService
             , private modalServise: BsModalService
@@ -59,8 +62,13 @@ export class EventosComponent implements OnInit {
   editarEvento(evento: Evento, template: any) {
     this.modoSalvar = 'put';
     this.openModal(template);
-    this.evento = evento;
-    this.registerForm.patchValue(evento);
+    this.evento = Object.assign({}, evento);
+    this.evento.imagemUrl = '';
+    this.evento.lotes = [];
+    this.evento.palestrante = [];
+    this.evento.redesSociais = [];
+    this.registerForm.patchValue(this.evento);
+    this.fileNameToUpdate = this.evento.imagemUrl.toString();
   }
 
   ngOnInit() {
@@ -102,10 +110,35 @@ export class EventosComponent implements OnInit {
     });
   }
 
+  uploadImagem() {
+
+    if (this.modoSalvar === 'post'){
+    const nomeArquivo = this.evento.imagemUrl.split('\\', 3);
+    this.evento.imagemUrl = nomeArquivo[2];
+
+    this.eventoService.postUpload(this.file, nomeArquivo[2]).subscribe(
+      () => {
+        this.dataAtual = new Date().getMilliseconds().toString();
+        this.getEventos();
+      }
+    );
+    } else {
+      this.evento.imagemUrl = this.fileNameToUpdate;
+      this.eventoService.postUpload(this.file, this.fileNameToUpdate).subscribe(
+        () => {
+          this.dataAtual = new Date().getMilliseconds().toString();
+          this.getEventos();
+        }
+      );
+    }
+  }
+
   salvarAlteracao(template: any) {
       if (this.registerForm.valid) {
         if (this.modoSalvar === 'post') {
           this.evento = Object.assign({}, this.registerForm.value);
+
+          this.uploadImagem();
           this.eventoService.postEvento(this.evento).subscribe(
             (novoEvento: Evento) => {
               this.toastrService.success('Evento editado com sucesso!');
@@ -117,6 +150,8 @@ export class EventosComponent implements OnInit {
           );
         } else {
           this.evento = Object.assign({id: this.evento.id}, this.registerForm.value);
+
+          this.uploadImagem();
           this.eventoService.putEvento(this.evento).subscribe(
             (novoEvento: Evento) => {
               this.toastrService.success('Evento editado com sucesso!');
@@ -146,6 +181,18 @@ export class EventosComponent implements OnInit {
         this.toastrService.error(`Erro ao excluir evento: ${error}`);
       }
     );
+  }
+
+  onFileChange(event) {
+
+    const reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      this.file = event.target.files;
+      console.log(this.file);
+    }
+
+    console.log(event);
   }
 
 
